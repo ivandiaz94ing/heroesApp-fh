e839d98d3153beb1ef69d6f9614c0b23ba91ba86
Ivan Arturo <ivandiaz94ing@gmail.com>
2024-01-26 21:47:10 -0500

    trabajando con angular material y backend

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    MaterialModule,
   ]
 })
 export class HeroesModule { }
---
+++ src/app/heroes/services/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Hero } from '../interfaces/hero.interface';
+import { environments } from 'src/environments/environments';
+
+@Injectable({providedIn: 'root'})
+export class HeroService {
+
+  constructor(private http: HttpClient) { }
+
+  private baseUrl : string = environments.baseUrl;
+
+  getHeroes():Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+
+  }
+
+}
